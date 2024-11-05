eb4e81341e3904e2cc1558053bbc43638862e1c3
cprofessional <xcjqzzn@gmail.com>
2024-11-04 21:19:49 -0600

    a

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@
 import FadeInSection from "@/components/miscellaneous/effects/fade-in/FadeIn";
 import ContactUs from "@/components/main-page/contact/Contact";
 import ExploreGod from "@/components/main-page/explore-god/ExploreGod";
-import Header from "@/components/global/header/Header";
+import Header from "@/components/globals/header/Header";
 import HelpOthers from "@/components/main-page/help-others/HelpOthers";
 import JoinUs from "@/components/main-page/join-us/JoinUs";
 import LatestNews from "@/components/main-page/news/News";
 import Welcome from "@/components/main-page/welcome/Welcome";
-import Footer from "@/components/global/footer/Footer";
+import Footer from "@/components/globals/footer/Footer";
 
 import styles from "./page.module.css";
 
